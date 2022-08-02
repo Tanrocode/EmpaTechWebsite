fe23daf2d211a5296e401682b590479bc6c1a925
Rishit Agrawal <rishigian1234@gmail.com>
2022-08-02 15:01:53 +0530

    debugged background and intro

--- app/src/components/intro.jsx
+++ app/src/components/intro.jsx
@@ -1,15 +1,14 @@
 import '../styles/intro.css'
+import HomePic from '../pictures/HomePic.png'
  
 export default function Intro() {
 
         return (
-            <div className="intro page">
-                <div>
-                    <span className="intro page ">Description for Website</span>
-
+            <div className="intro page" style={{backgroundImage:`url(${HomePic})` }}>
                     
-                    
-                </div>
+                <span className="TextStyle intro  ">Illuminating Our Community, One Project at a Time</span>
+                <span className="center  StartStyle"><button className="start">Learn More</button></span>
+             
             </div>
         );
 
