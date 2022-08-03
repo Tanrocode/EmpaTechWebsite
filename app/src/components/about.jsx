e3f20e21908374e9ba8f5e9427d7120902b8bda6
Rishit Agrawal <rishigian1234@gmail.com>
2022-08-04 00:23:56 +0530

    Added Cards

---
+++ app/src/components/about.jsx
@@ -0,0 +1,27 @@
+import "../styles/about.css";
+import HomePic from "../pictures/HomePic.png";
+import React, { Component } from "react";
+
+export default function About() {
+  return (
+      <div>
+        <div className="page about ">
+            <div className="shadow">
+                 <span>
+                    About
+                </span>
+            </div>
+        </div>
+
+        <div className="page about2 " >
+            <div className="shadow">
+                <span>
+                    About2
+                </span>
+            </div>
+            
+        </div>
+        
+    </div>
+  );
+}
--- app/src/components/intro.jsx
+++ app/src/components/intro.jsx
@@ -1,16 +1,20 @@
 import "../styles/intro.css";
 import HomePic from "../pictures/HomePic.png";
+import About from "./about.jsx";
 import React, { Component } from "react";
 
 export default function Intro() {
   return (
-    <div className="intro page" style={{ backgroundImage: `url(${HomePic})` }}>
-      <span className="TextStyle intro  ">
-        Illuminating Our Community, One Project at a Time
-      </span>
-      <span className="center  StartStyle">
-        <button className="start">Learn More</button>
-      </span>
+    <div>
+        <div className="intro page" style={{ backgroundImage: `url(${HomePic})` }}>
+            <span className="TextStyle intro  ">
+                Illuminating Our Community, One Project at a Time
+            </span>
+            <span className="center  StartStyle">
+                <button className="start">Learn More</button>
+            </span>
+        </div>
+        <About/>
     </div>
   );
 }
