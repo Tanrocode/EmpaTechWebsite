7e5fbb1891748cdb766314156f08600a0c35976a
Rishit Agrawal <rishigian1234@gmail.com>
2022-08-08 15:42:44 -0700

    card removed

--- app/src/components/about.jsx
+++ app/src/components/about.jsx
@@ -2,13 +2,12 @@ import "../styles/about.scss";
 import React from "react";
 
 export default function About(props) {
-  const titleClass = "title " + props.title;
   const className = "page " + props.className;
   return (
     <div>
       <div className={className}>
-        <div className="shadow">
-          <h1 className={titleClass}> {props.title}</h1>
+        <div className="align-card">
+          <h1 className="title"> {props.title}</h1>
           <span>{props.children}</span>
         </div>
       </div>
